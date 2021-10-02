f9e240f6c78eda5d43b8b4269bfacd1635eb6563
David <david@villacis.net>
2021-10-02 17:40:45 -0500

    fixed ds costs

--- Learning/cost.py
+++ Learning/cost.py
@@ -1,4 +1,6 @@
 import numpy as np
+from skimage.metrics import structural_similarity as ssim
+from skimage.metrics import peak_signal_noise_ratio as psnr
 
 def l2_cost(original,reconstruction):
     return 0.5*np.linalg.norm(original.ravel()-reconstruction.ravel())**2
@@ -7,5 +9,17 @@ def l2_cost_ds(ds_denoised):
     cost = 0
     for img in ds_denoised.keys():
         cost += l2_cost(ds_denoised[img][0],ds_denoised[img][2])
-    return cost
+    return cost/len(ds_denoised)
+
+def ssim_cost_ds(ds_denoised):
+    cost = 0
+    for img in ds_denoised.keys():
+        cost += ssim(ds_denoised[img][0],ds_denoised[img][2])
+    return cost/len(ds_denoised)
+
+def psnr_cost_ds(ds_denoised):
+    cost = 0
+    for img in ds_denoised.keys():
+        cost += psnr(ds_denoised[img][0],ds_denoised[img][2])
+    return cost/len(ds_denoised)
     
--- test/test_learning.py
+++ test/test_learning.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from Learning.cost import l2_cost,l2_cost_ds
+from Learning.cost import l2_cost,l2_cost_ds, psnr_cost_ds, ssim_cost_ds
 from TVDenoising.scalar_denoising import denoise_ds
 
 class LearningTest(unittest.TestCase):
@@ -16,4 +16,6 @@ class LearningTest(unittest.TestCase):
     def test_ds_l2cost(self):
         den_ds = denoise_ds('datasets/faces_train_128_10/filelist.txt',20,1.0)
         cost = l2_cost_ds(den_ds)
-        print(cost)
+        ssim = ssim_cost_ds(den_ds)
+        psnr = psnr_cost_ds(den_ds)
+        print(f'l2:{cost} ssim:{ssim} psnr:{psnr}')
