8df74d695b4a47389db53ebef6e80f9088460ea8
David Villacis <david@villacis.net>
2021-10-29 16:57:27 -0500

    version 1 of tgamma

--- Operators/Tgamma.py
+++ Operators/Tgamma.py
@@ -1,22 +1,35 @@
 import numpy as np
-from pylops import LinearOperator
-from Operators.norms import pointwise_euclidean_norm
+from pylops import LinearOperator, FirstDerivative
 
-def phi(u,gamma):
-    nu = pointwise_euclidean_norm(u)
-    return np.concatenate((np.where(nu<gamma,-nu/gamma**2+2/gamma,1/nu),np.where(nu<gamma,-nu/gamma**2+2/gamma,1/nu)))
+def phi(nu,gamma):
+    return np.where(nu<gamma,-nu/gamma**2+2/gamma,1/nu)
 
-def psi(u,gamma):
-    nu = pointwise_euclidean_norm(u)
-    return np.concatenate((np.where(nu<gamma,-1/nu/gamma**2,-1/nu**3),np.where(nu<gamma,-1/nu/gamma**2,-1/nu**3)))
+def psi(nu,gamma):
+    return np.where(nu<gamma,-1/nu/gamma**2,-1/nu**3)
 
 class Tgamma(LinearOperator):
     def __init__(self, u, gamma=1e-3, dtype='float64'):
-        self.u = u
-        self.phi_u = phi(u,gamma)
-        self.psi_u = psi(u,gamma)
-        self.shape = (len(u),len(u))
+        n = len(u)
+        self.Kx = FirstDerivative(n,kind='centered',dir=0,edge=True)
+        self.Ky = FirstDerivative(n,kind='centered',dir=1,edge=True)
+        self.Kxu = self.Kx*u
+        self.Kyu = self.Ky*u
+        self.Kxu2 = self.Kxu * self.Kxu
+        self.Kyu2 = self.Kyu * self.Kyu
+        self.Kxyu = self.Kxu * self.Kyu
+        self.nKu = np.linalg.norm(np.vstack((self.Kxu,self.Kyu)).T,axis=1)
+        print(self.nKu)
+        self.phi_u = phi(self.nKu,gamma)
+        self.psi_u = psi(self.nKu,gamma)
+        print(f'phi_u:\n{self.phi_u}\npsi_u:\n{self.psi_u}')
+        self.shape = (2*len(u),len(u))
         self.dtype = dtype
 
     def _matvec(self,x):
-        return self.phi_u * x + self.psi_u * self.u * self.u * x
+        Kxx = self.Kx * x
+        Kyx = self.Ky * x
+        print(self.phi_u * Kxx)
+        print(self.psi_u * (self.Kxu2 * Kxx + self.Kxyu * Kyx))
+        a = self.psi_u * (self.Kxu2 * Kxx + self.Kxyu * Kyx) + self.phi_u * Kxx
+        b = self.psi_u * (self.Kyu2 * Kyx + self.Kxyu * Kxx) + self.phi_u * Kyx
+        return np.concatenate((a,b))
--- test/test_Tgamma.py
+++ test/test_Tgamma.py
@@ -5,15 +5,11 @@ from Operators.Tgamma import Tgamma
 
 class TestTgamma(unittest.TestCase):
     def test_tgamma(self):
-        np.random.seed(12345)
+        np.random.seed(12347)
         n = 12
-        u = np.random.rand(n,n)
-        K = Gradient(dims=(n,n),kind='forward')
-        Ku = K*u.ravel()
-        print(Ku.shape)
-        w = np.ones(len(Ku))
-        T = Tgamma(Ku,gamma=1e-5)
+        u = np.random.randn(n,n)
+        T = Tgamma(u.ravel(),gamma=1e-5)
+        w = np.random.randn(144)
         print(T)
-        print(T*K)
         print(T.shape)
         print(T*w)
