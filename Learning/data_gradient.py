fixed smooth gradient

Learning/data_gradient.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pylops, pyproximal
+from Operators.Tgamma import Tgamma
 
 from Operators.operators import ActiveOp, InactiveOp
 from Operators.patch import patch, reverse_patch
@@ -44,14 +45,20 @@ def smooth_scalar_data_adjoint(original,reconstruction,data_parameter,show=False
     L = pylops.Diagonal(data_parameter * np.ones(n))
     Id = pylops.Identity(2*n)
     Z = pylops.Zero(n)
-    Act = ActiveOp(reconstruction)
-    Inact = InactiveOp(reconstruction)
-    A = pylops.Block([[L,K.adjoint()],[Act*K-Inact*K,Inact]])
+    Tg = Tgamma(reconstruction.ravel())
+    A = pylops.Block([[L,K.adjoint()],[-Tg,Id]])
     b = np.concatenate((reconstruction.ravel()-original.ravel(),np.zeros(2*n)),axis=0)
     p = pylops.optimization.solver.cg(A,b,np.zeros_like(b))
     if show==True:
         print(p[1:])
     return p[0][:n]
+
+def smooth_scalar_data_gradient(original,noisy,reconstruction,data_parameter):
+    p = smooth_scalar_data_adjoint(original,reconstruction,data_parameter)
+    #L = pylops.Diagonal(p[0])
+    #grad = L*(reconstruction.ravel()-noisy.ravel())
+    grad = -np.dot(p,reconstruction.ravel()-noisy.ravel())
+    return grad
 
 
 # PATCH

Operators/Tgamma.py
@@ -8,7 +8,7 @@ def psi(nu,gamma):
     return np.where(nu<gamma,-1/nu/gamma**2,-1/nu**3)
 
 class Tgamma(LinearOperator):
-    def __init__(self, u, gamma=1e-3, dtype='float64'):
+    def __init__(self, u, gamma=1e-10, dtype='float64'):
         n = len(u)
         self.Kx = FirstDerivative(n,kind='centered',dir=0,edge=True)
         self.Ky = FirstDerivative(n,kind='centered',dir=1,edge=True)
@@ -18,18 +18,18 @@ class Tgamma(LinearOperator):
         self.Kyu2 = self.Kyu * self.Kyu
         self.Kxyu = self.Kxu * self.Kyu
         self.nKu = np.linalg.norm(np.vstack((self.Kxu,self.Kyu)).T,axis=1)
-        print(self.nKu)
+        #print(f'grad:\n{self.nKu}')
         self.phi_u = phi(self.nKu,gamma)
         self.psi_u = psi(self.nKu,gamma)
-        print(f'phi_u:\n{self.phi_u}\npsi_u:\n{self.psi_u}')
+        #print(f'phi_u:\n{self.phi_u}\npsi_u:\n{self.psi_u}')
         self.shape = (2*len(u),len(u))
         self.dtype = dtype
 
     def _matvec(self,x):
         Kxx = self.Kx * x
         Kyx = self.Ky * x
-        print(self.phi_u * Kxx)
-        print(self.psi_u * (self.Kxu2 * Kxx + self.Kxyu * Kyx))
+        #print(self.phi_u * Kxx)
+        #print(self.psi_u * (self.Kxu2 * Kxx + self.Kxyu * Kyx))
         a = self.psi_u * (self.Kxu2 * Kxx + self.Kxyu * Kyx) + self.phi_u * Kxx
         b = self.psi_u * (self.Kyu2 * Kyx + self.Kxyu * Kxx) + self.phi_u * Kyx
         return np.concatenate((a,b))

test/test_Tgamma.py
@@ -6,10 +6,25 @@ from Operators.Tgamma import Tgamma
 class TestTgamma(unittest.TestCase):
     def test_tgamma(self):
         np.random.seed(12347)
-        n = 12
-        u = np.random.randn(n,n)
-        T = Tgamma(u.ravel(),gamma=1e-5)
-        w = np.random.randn(144)
+        t = 12
+        noise_level = 0.01
+        sz = (t,t)
+        # lu = int(sz[0]/2 - sz[0]/4)
+        # br = int(lu + sz[0]/3)
+        # image = np.ones(sz)
+        # x = np.linspace(0, 1, t//3)
+        # image[lu:br, lu:br] = np.tile(x, (t//3, 1))
+        # noise = np.random.randn(sz[0], sz[1])
+        # noisy = image + noise_level * noise
+        # noisy = np.clip(noisy, 0.0, 1.0)
+        lu = int(sz[0]/2 - sz[0]/4)
+        br = int(lu + sz[0]/3)
+        image = np.ones(sz)
+        image[lu:br,lu:br] = 1e-5
+        noisy = image + noise_level * np.random.randn(t,t)
+        noisy = np.clip(noisy,0.0,1.0)
+        T = Tgamma(noisy.ravel(),gamma=1e-3)
+        w = np.random.randn(t*t)
         print(T)
         print(T.shape)
         print(T*w)