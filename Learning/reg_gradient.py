reg gradient fixes

Learning/reg_gradient.py
@@ -16,24 +16,32 @@ def scalar_reg_adjoint(original,reconstruction,reg_parameter,show=False):
     L = pylops.Diagonal(reg_parameter * np.ones(2*n))
     Id2 = pylops.Identity(2*n)
     Z = pylops.Zero(n)
-    #Act = ActiveOp(reconstruction)
+    Act = ActiveOp(reconstruction)
     T = TOp(reconstruction.ravel())
     Inact = InactiveOp(reconstruction)
-    A = pylops.Block([[Id,K.adjoint()],[T,Inact]])
+    A = pylops.Block([[Id,K.adjoint()],[-L*T,Inact + 100*Act]])
     b = np.concatenate((reconstruction.ravel()-original.ravel(),np.zeros(2*n)),axis=0)
-    p = pylops.optimization.solver.cg(A,b,np.zeros_like(b),niter=100)
+    p = pylops.optimization.solver.cg(A,b,np.zeros_like(b),tol=1e-4)
     if show==True:
         print(f'res:{np.linalg.norm(A*p[0]-b)}')
         print(f'cg_out: {p[1:]}')
     adj = p[0][:n]
-    Ku = Inact*K*reconstruction.ravel()
-    den = np.vstack([pointwise_euclidean_norm(Ku)]*2).ravel()
-    den[den==0]=0.01
-    Ku = Ku / den
-    return -np.dot(K*adj,Ku)
+    return adj
 
-def scalar_reg_gradient(original,noisy,reconstruction,reg_parameter,show=False):
-    return scalar_reg_adjoint(original,reconstruction,reg_parameter,show=show)
+def scalar_reg_gradient(original,noisy,reconstruction,reg_parameter,show=False,tol=1e-7):
+    nx,ny = original.shape
+    n = nx*ny
+    p = scalar_reg_adjoint(original,reconstruction,reg_parameter,show=show)
+    Kx = FirstDerivative(n,kind='centered',dir=0,edge=True)
+    Ky = FirstDerivative(n,kind='centered',dir=1,edge=True)
+    Kxu = Kx*reconstruction.ravel()
+    Kyu = Ky*reconstruction.ravel()
+    Kxp = Kx*p
+    Kyp = Ky*p
+    nKu = np.linalg.norm(np.vstack((Kxu,Kyu)).T,axis=1)
+    mul = np.where(nKu<tol,0,1/nKu)
+    grad = mul * (Kxu * Kxp + Kyu * Kyp)
+    return -np.sum(grad)
 
 def scalar_reg_gradient_ds(ds_denoised,reg_parameter):
     grad = 0

TVDenoising/scalar_denoising.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 import pylops, pyproximal
 
-from Dataset.load_dataset import load_ds_file
+from Dataset.load_dataset import load_ds_file, open_image
 
 def denoise(noisy,data_parameter,reg_parameter,niter=100,show=False):
     nx,ny = noisy.shape
@@ -20,10 +20,8 @@ def denoise_ds(dsfile,data_parameter,reg_parameter,niter=100,show=False):
     ds = load_ds_file(dsfile)
     reconstruction = {}
     for img in ds.keys():
-        original = np.array(Image.open(img.strip()))
-        original = original / np.max(original)
-        noisy = np.array(Image.open(ds[img].strip()))
-        noisy = noisy / np.max(noisy)
+        original = open_image(img.strip())
+        noisy = open_image(ds[img].strip())
         rec = denoise(noisy,data_parameter,reg_parameter,niter=niter,show=show)
         reconstruction.update({img:(original,noisy,rec)})
     return reconstruction

regularization_learning.py
@@ -1,9 +1,12 @@
 import argparse
+import warnings
 import os
 import sys
 import numpy as np
 
-from Learning.optimization import find_optimal_data_scalar, find_optimal_data_patch
+warnings.filterwarnings('ignore')
+
+from Learning.optimization import find_optimal_reg_scalar
 from Report.output_report import write_scalar_report, write_patch_report
 
 parser = argparse.ArgumentParser(prog='bilevel_regularization_learning',description='Bilevel Regularization Parameter Learning',epilog='Enjoy!')
@@ -49,16 +52,17 @@ if parameter_type == 'scalar':
             print(f'Directory {report_dir} created succesfully...')
         write_scalar_report(report_dir,optimal,optimal_ds)
 else:
-    paramater_initial_value = paramater_initial_value * np.ones((patch_size,patch_size))
-    optimal,optimal_ds = find_optimal_reg_patch(dataset_file,paramater_initial_value,show=show)
-    if optimal.success == True:
-        print(f'Optimal parameter found:\n{optimal.x}')
-    else:
-        print(f'{optimal.message}')
-    if report_dir != None:
-        if not os.path.isdir(report_dir):
-            os.makedirs(report_dir)
-            print(f'Directory {report_dir} created succesfully...')
-        write_patch_report(report_dir,optimal,optimal_ds)
+    # paramater_initial_value = paramater_initial_value * np.ones((patch_size,patch_size))
+    # optimal,optimal_ds = find_optimal_reg_patch(dataset_file,paramater_initial_value,show=show)
+    # if optimal.success == True:
+    #     print(f'Optimal parameter found:\n{optimal.x}')
+    # else:
+    #     print(f'{optimal.message}')
+    # if report_dir != None:
+    #     if not os.path.isdir(report_dir):
+    #         os.makedirs(report_dir)
+    #         print(f'Directory {report_dir} created succesfully...')
+    #     write_patch_report(report_dir,optimal,optimal_ds)
+    print('Not implemented')
 
 

test/test_reg_gradient.py
@@ -1,4 +1,5 @@
 import unittest
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,7 @@ from Learning.reg_gradient import scalar_reg_gradient
 
 class TestGradient(unittest.TestCase):
     def test_smooth_scalar_reg_gradient(self):
+        warnings.filterwarnings('ignore')
         orig,noisy = get_image_pair('datasets/cameraman_128_5/filelist.txt',0)
         # np.random.seed(12347)
         # t = 12
@@ -21,7 +23,7 @@ class TestGradient(unittest.TestCase):
         # noisy = orig + noise_level * np.random.randn(t,t)
         # noisy = np.clip(noisy,0.0,1.0)
         
-        par = np.arange(0.008,0.03,step=0.001)
+        par = np.arange(1e-7,0.02,step=1e-3)
         grads=[]
         costs =[]
         for p in par:

test/test_reg_gradient_smooth.py
@@ -22,7 +22,7 @@ class TestGradient(unittest.TestCase):
         # noisy = orig + noise_level * np.random.randn(t,t)
         # noisy = np.clip(noisy,0.0,1.0)
         
-        par = np.arange(0.3,0.4,step=0.005)
+        par = np.arange(0.3,0.5,step=0.005)
         grads=[]
         costs =[]
         for p in par: