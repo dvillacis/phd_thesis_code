bf34d23b5a7632ac83430b88a112e5ea47a6415e
David <david@villacis.net>
2021-11-19 10:08:44 -0500

    added dogbox

--- test/test_nstr.py
+++ test/test_nstr.py
@@ -2,13 +2,20 @@ import unittest
 import warnings
 import numpy as np
 
-from TrustRegion.nstr import nstr
-from TrustRegion.step_finder import cauchy_point_step_finder
+from TrustRegion.nsdogbox import nsdogbox
+from TrustRegion.step_finder import dogleg_step_finder,cauchy_point_step_finder
 
 class TestNSTR(unittest.TestCase):
     def test_nstr(self):
         warnings.filterwarnings('ignore')
-        sol = nstr(lambda x:np.dot(x,x)+5*x,lambda x:2*x+5,np.array([3.0,3.0]),step_finder=cauchy_point_step_finder,verbose=True)
+        A = np.array([[1.0,0.0],[0.0,0.5]])
+        v = np.array([1.0,1.0])
+        fun = lambda x:0.5*np.dot(x,np.dot(A,x))+np.dot(v,x)
+        grad = lambda x: np.dot(A,x)+v
+        x0 = np.array([300.0,300.0])
+        lb = np.array([0.0,0.0])
+        ub = np.array([1000.0,1000.0])
+        sol = nsdogbox(fun,grad,x0,lb,ub,verbose=2)
         print(sol)
 
         
