fixes on the reg gradient calculation

Learning/optimization.py
@@ -53,7 +53,7 @@ def find_optimal_reg_scalar(dsfile,initial_reg_parameter,show=False):
         iprint = 2
     # bnds = scipy.optimize.Bounds(1e-10,np.inf)
     # optimal = scipy.optimize.minimize(fun=lambda x: reg_cost_fn_scalar(dsfile,x),jac=lambda x:reg_gradient_fn_scalar(dsfile,x),hess=scipy.optimize.SR1(),x0=initial_reg_parameter,method='trust-constr',bounds=bnds,options={'verbose':iprint,'gtol':1e-6})
-    optimal = nsdogbox(fun=lambda x: reg_cost_fn_scalar(dsfile,x),grad=lambda x:reg_gradient_fn_scalar(dsfile,x),reg_grad=lambda x:reg_gradient_fn_scalar(dsfile,x),x0=np.array([initial_reg_parameter]),verbose=iprint,initial_radius=1.0)
+    optimal = nsdogbox(fun=lambda x: reg_cost_fn_scalar(dsfile,x),grad=lambda x:reg_gradient_fn_scalar(dsfile,x),reg_grad=lambda x:reg_gradient_fn_scalar(dsfile,x),x0=np.array([initial_reg_parameter]),verbose=iprint)
     if show == True:
         print(optimal)
     optimal_ds = denoise_ds(dsfile,data_parameter=1.0,reg_parameter=optimal.x)
@@ -103,11 +103,12 @@ def reg_gradient_fn_patch(dsfile,reg_parameter):
     return patch_reg_gradient_ds(den_ds,reg_parameter)
 
 def find_optimal_reg_patch(dsfile,initial_reg_parameter,show=False):
-    iprint = -1
+    iprint = 0
     if show == True:
-        iprint = 100
-    bnds = scipy.optimize.Bounds(0.001*np.ones(initial_reg_parameter.ravel().shape),[np.inf]*len(initial_reg_parameter.ravel()))
-    optimal = scipy.optimize.minimize(fun=lambda x: reg_cost_fn_patch(dsfile,x),jac=lambda x:reg_gradient_fn_patch(dsfile,x),hess=scipy.optimize.SR1(),x0=initial_reg_parameter.ravel(),method='trust-constr',bounds=bnds,options={'verbose':iprint,'gtol':1e-6})
+        iprint = 2
+    #bnds = scipy.optimize.Bounds(0.001*np.ones(initial_reg_parameter.ravel().shape),[np.inf]*len(initial_reg_parameter.ravel()))
+    #optimal = scipy.optimize.minimize(fun=lambda x: reg_cost_fn_patch(dsfile,x),jac=lambda x:reg_gradient_fn_patch(dsfile,x),hess=scipy.optimize.SR1(),x0=initial_reg_parameter.ravel(),method='trust-constr',bounds=bnds,options={'verbose':iprint,'gtol':1e-6})
+    optimal = nsdogbox(fun=lambda x: reg_cost_fn_patch(dsfile,x),grad=lambda x:reg_gradient_fn_patch(dsfile,x),reg_grad=lambda x:reg_gradient_fn_patch(dsfile,x),x0=initial_reg_parameter.ravel(),verbose=iprint)
     if show == True:
         print(optimal)
     x = optimal.x

Learning/reg_gradient.py
@@ -8,30 +8,30 @@ from Operators.patch import patch, reverse_patch
 from Operators.norms import pointwise_euclidean_norm
 from Operators.Tgamma import Tgamma
 
-def scalar_reg_adjoint(original,reconstruction,reg_parameter,show=False):
+def scalar_reg_adjoint(original,reconstruction,reg_parameter,show=False,tol=1e-10):
     nx,ny = original.shape
     n = nx*ny
-    K = pylops.Gradient(dims=(nx,ny),kind='forward')
+    K = pylops.Gradient(dims=(nx,ny),kind='centered',edge=True)
     Id = pylops.Identity(n)
     L = pylops.Diagonal(reg_parameter * np.ones(2*n))
     Id2 = pylops.Identity(2*n)
     Z = pylops.Zero(n)
     Act = ActiveOp(reconstruction)
-    T = TOp(reconstruction.ravel())
+    T = TOp(reconstruction.ravel(),tol=tol)
     Inact = InactiveOp(reconstruction)
-    A = pylops.Block([[Id,K.adjoint()],[-L*Inact*T,Inact+1e-5*Act]])
+    A = pylops.Block([[Id,K.adjoint()],[-L*Inact*T,Inact+1e-4*Act]])
     b = np.concatenate((reconstruction.ravel()-original.ravel(),np.zeros(2*n)),axis=0)
-    p = pylops.optimization.solver.cg(A,b,np.zeros_like(b),tol=1e-4)
+    p = pylops.optimization.solver.cg(A,b,np.zeros_like(b),tol=1e-10)
     if show==True:
         print(f'res:{np.linalg.norm(A*p[0]-b)}')
         print(f'cg_out: {p[1:]}')
     adj = p[0][:n]
     return adj
 
-def scalar_reg_gradient(original,noisy,reconstruction,reg_parameter,show=False,tol=1e-7):
+def scalar_reg_gradient(original,noisy,reconstruction,reg_parameter,show=False,tol=1e-10):
     nx,ny = original.shape
     n = nx*ny
-    p = scalar_reg_adjoint(original,reconstruction,reg_parameter,show=show)
+    p = scalar_reg_adjoint(original,reconstruction,reg_parameter,show=show,tol=tol)
     Kx = FirstDerivative(n,kind='centered',dir=0,edge=True)
     Ky = FirstDerivative(n,kind='centered',dir=1,edge=True)
     Kxu = Kx*reconstruction.ravel()
@@ -39,7 +39,7 @@ def scalar_reg_gradient(original,noisy,reconstruction,reg_parameter,show=False,t
     Kxp = Kx*p
     Kyp = Ky*p
     nKu = np.linalg.norm(np.vstack((Kxu,Kyu)).T,axis=1)
-    mul = np.where(nKu<tol,0,1/nKu)
+    mul = np.where(nKu<tol,0,1.0/nKu)
     grad = mul * (Kxu * Kxp + Kyu * Kyp)
     return -np.sum(grad)
 
@@ -104,7 +104,7 @@ def patch_reg_adjoint(original,reconstruction,reg_parameter:np.ndarray,show=Fals
     Act = ActiveOp(reconstruction)
     T = TOp(reconstruction.ravel())
     Inact = InactiveOp(reconstruction)
-    A = pylops.Block([[Id,K.adjoint()],[-L*T,Inact + 1*Act]])
+    A = pylops.Block([[Id,K.adjoint()],[-L*T,Inact + 1e-5*Act]])
     b = np.concatenate((reconstruction.ravel()-original.ravel(),np.zeros(2*n)),axis=0)
     p = pylops.optimization.solver.cg(A,b,np.zeros_like(b),tol=1e-4)
     if show==True:

Operators/TOp.py
@@ -2,18 +2,16 @@ import numpy as np
 from pylops import LinearOperator, FirstDerivative
 
 def phi(nu,tol):
-    absnu = np.abs(nu)
-    return np.where(absnu<tol,1,1/absnu)
+    return np.where(nu<tol,1,-1/nu)
 
 def psi(nu,tol):
-    absnu = np.abs(nu)
-    return np.where(absnu<tol,0,-1/absnu**3)
+    return np.where(nu<tol,0,1/nu**3)
 
 class TOp(LinearOperator):
-    def __init__(self, u, tol=1e-10, dtype='float64'):
+    def __init__(self, u, tol=1e-4, dtype='float64'):
         n = len(u)
-        self.Kx = FirstDerivative(n,kind='centered',dir=0,edge=True)
-        self.Ky = FirstDerivative(n,kind='centered',dir=1,edge=True)
+        self.Kx = FirstDerivative(n,kind='centered',dir=0,edge=False)
+        self.Ky = FirstDerivative(n,kind='centered',dir=1,edge=False)
         self.Kxu = self.Kx*u
         self.Kyu = self.Ky*u
         self.Kxu2 = self.Kxu * self.Kxu

Operators/Tgamma.py
@@ -3,11 +3,11 @@ from pylops import LinearOperator, FirstDerivative
 
 def phi(nu,gamma):
     absnu = np.abs(nu)
-    return np.where(absnu<gamma,gamma,1/absnu)
+    return np.where(absnu<gamma,-absnu/gamma**2+2/gamma,1/absnu)
 
 def psi(nu,gamma):
     absnu = np.abs(nu)
-    return np.where(absnu<gamma,0,-1/absnu**3)
+    return np.where(absnu<gamma,-1/(absnu+gamma)/gamma**2,-1/absnu**3)
 
 class Tgamma(LinearOperator):
     def __init__(self, u, gamma=1e-5, dtype='float64'):

test/test_reg_gradient.py
@@ -23,11 +23,11 @@ class TestGradient(unittest.TestCase):
         # noisy = orig + noise_level * np.random.randn(t,t)
         # noisy = np.clip(noisy,0.0,1.0)
         
-        par = np.arange(0.01,0.1,step=0.5e-2)
+        par = np.arange(0.0152,0.0157,step=0.1e-4)
         grads=[]
         costs =[]
         for p in par:
-            rec = denoise(noisy,1.0,p,niter=5000)
+            rec = denoise(noisy,1.0,p,niter=10000)
             c = l2_cost(orig,rec)
             g = scalar_reg_gradient(orig,noisy,rec,p,show=True)
             grads.append(g)

test/test_reg_gradient_smooth.py
@@ -11,11 +11,11 @@ from Learning.reg_gradient import smooth_scalar_reg_gradient
 class TestGradient(unittest.TestCase):
     def test_smooth_scalar_reg_gradient(self):
         orig,noisy = get_image_pair('datasets/cameraman_128_5/filelist.txt',0)
-        par = np.arange(1e-7,0.08,step=0.09e-2)
+        par = np.arange(0.0152,0.0157,step=0.1e-4)
         grads=[]
         costs =[]
         for p in par:
-            rec = denoise(noisy,1.0,p,niter=5000)
+            rec = denoise(noisy,1.0,p,niter=10000)
             c = l2_cost(orig,rec)
             g = smooth_scalar_reg_gradient(orig,noisy,rec,p,show=True)
             grads.append(g)