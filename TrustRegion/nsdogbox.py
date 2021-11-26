1cfcd4d92f5dadce40035a3f50eda9111f4e120b
David Villacis <david@villacis.net>
2021-11-26 18:16:36 -0500

    minor fix

--- TrustRegion/nsdogbox.py
+++ TrustRegion/nsdogbox.py
@@ -30,7 +30,7 @@ def print_header_dogbox():
 def nsdogbox(fun,grad,reg_grad,x0,lb=None,ub=None,initial_radius=None,threshold_radius=1e-3,verbose=0,xtol=1e-8,ftol=1e-8,gtol=1e-5,max_nfev=10000):
 
     if not lb:
-        lb = 1e-8*np.ones_like(x0)
+        lb = 1e-12*np.ones_like(x0)
     if not ub:
         ub = np.ones_like(x0) * np.inf
 
